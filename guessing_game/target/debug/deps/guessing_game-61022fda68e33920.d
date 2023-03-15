460644a1cc95c6120be1b573daf2377472290df1
kkaizer11 <kaizerin.m@gmail.com>
2023-03-15 19:17:34 -0300

    enchendo linguiça

---
+++ guessing_game/target/debug/deps/guessing_game-61022fda68e33920.d
@@ -0,0 +1,5 @@
+/home/kkaizer/Documents/rustPL/guessing_game/target/debug/deps/guessing_game-61022fda68e33920: src/main.rs
+
+/home/kkaizer/Documents/rustPL/guessing_game/target/debug/deps/guessing_game-61022fda68e33920.d: src/main.rs
+
+src/main.rs:
---
+++ guessing_game/target/debug/guessing_game.d
@@ -0,0 +1,1 @@
+/home/kkaizer/Documents/rustPL/guessing_game/target/debug/guessing_game: /home/kkaizer/Documents/rustPL/guessing_game/src/main.rs
